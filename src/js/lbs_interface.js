2f0dca6befd04013f648a6513a3d350998d0edaf
camelonrye <camelonrye@gmail.com>
2017-08-25 20:45:54 -0400

    added some example POIs

--- src/js/lbs_interface.js
+++ src/js/lbs_interface.js
@@ -7,4 +7,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoibndlYnoiLCJhIjoiY2lsc3NvMWI4MDA2MHUwa3NhZ2xvejJqdiJ9.3aR1Ye5KYmMMGgrUeBvYkw'
 }).addTo(mymap);
 
+var skatespots_phl = "https://gist.githubusercontent.com/nwebz/cfd639436c6f4bbbb27f1935ff3adc09/raw/da9fb6e963588b3568c31daf150268713a96d7ee/map.geojson"
+
+$.getJSON(skatespots_phl, function(data) {
+   L.geoJson(data).addTo(mymap); 
+    });
+
 L.control.locate().addTo(mymap);
